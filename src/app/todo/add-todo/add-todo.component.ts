7bf13cd265a34bbd699c18870022c7460be3a530
giedeve <gieczarny@gmail.com>
2021-06-04 22:38:09 +0200

    add three components todo

---
+++ src/app/todo/add-todo/add-todo.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.scss']
+})
+export class AddTodoComponent implements OnInit {
+  task:string;
+  @Output() addTodo : EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // adds the todo in the list
+  saveTask() {
+    const todo =
+    {
+      title : this.task,
+      completed:false
+    }
+    this.addTodo.emit(todo);
+  }
+}

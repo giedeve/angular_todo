32974d925fd874e6fa57b8e1a32293ae9bde0556
giedeve <gieczarny@gmail.com>
2021-06-04 22:40:22 +0200

    fix bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+// import { ErrorPageComponent } from './error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'todo', component: NavigationComponent},
+  // { path: '**', component: ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { TodosComponent } from './todo/todos/todos.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { AddTodoComponent } from './todo/add-todo/add-todo.component';
+// import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavigationComponent, TodosComponent, TodoItemComponent, AddTodoComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
